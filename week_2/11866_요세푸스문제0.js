104ed039208154e169c3fb60f81ed31cd973b6d8
sion <noismik97@gmail.com>
2024-09-13 14:08:37 +0900

    solve

--- week_2/11866_요세푸스문제0.js
+++ week_2/11866_요세푸스문제0.js
@@ -1,1 +1,19 @@
 // https://www.acmicpc.net/problem/11866
+
+const FILE_PATH = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(FILE_PATH).toString().trim().split(' ').map(Number);
+
+const [n, k] = input;
+
+const queue = Array.from(Array(n), (_, i) => i + 1);
+
+const answer = [];
+
+let cursor = 0;
+while (queue.length) {
+  cursor = (cursor + k - 1) % queue.length;
+  answer.push(queue[cursor]);
+  queue.splice(cursor, 1);
+}
+
+console.log(`<${answer.join(', ')}>`);
--- week_2/3986_좋은단어.js
+++ week_2/3986_좋은단어.js
@@ -1,1 +1,21 @@
 // https://www.acmicpc.net/problem/3986
+
+const FILE_PATH = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(FILE_PATH).toString().trim().split('\n');
+
+const [n, ...list] = input;
+let count = 0;
+
+for (let i = 0; i < n; i++) {
+  const stack = [];
+  const row = list[i].split('');
+
+  for (const char of row) {
+    const top = stack[stack.length - 1];
+    top === char ? stack.pop() : stack.push(char);
+  }
+
+  stack.length === 0 && count++;
+}
+
+console.log(count);
