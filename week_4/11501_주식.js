ac9cbf30d869314ee6e5fbd3d7979c90c0954c2c
sion <noismik97@gmail.com>
2024-09-27 18:18:28 +0900

    solve

--- week_4/11501_주식.js
+++ week_4/11501_주식.js
@@ -1,1 +1,27 @@
 // https://www.acmicpc.net/problem/11501
+
+const FILE_PATH = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(FILE_PATH).toString().split('\n');
+
+let T = +input.shift();
+const answer = [];
+
+while (T--) {
+  const N = +input.shift(); // 날의 수
+  const stock = input.shift().split(' ').map(Number); // 날짜 별 주가
+
+  let profit = 0; // 최대이득
+  let highest = -1;
+
+  for (let i = N - 1; 0 <= i; i--) {
+    if (stock[i] < highest) {
+      profit += highest - stock[i];
+    } else if (stock[i] > highest) {
+      highest = stock[i];
+    }
+  }
+
+  answer.push(profit);
+}
+
+console.log(answer.join('\n'));
--- week_4/2805_나무자르기.js
+++ week_4/2805_나무자르기.js
@@ -1,1 +1,19 @@
 // https://www.acmicpc.net/problem/2805
+
+const FILE_PATH = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(FILE_PATH).toString().split('\n');
+
+// 나무의 수 N, 나무의 길이 M
+const [N, M] = input.shift().split(' ').map(Number);
+const logs = input
+  .shift()
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+console.log(logs);
+
+let st = 0;
+let en = 0;
+
+let mid = Math.ceil((st + en) / 2);
